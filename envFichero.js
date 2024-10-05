acb1e8fab1c0ed8d0acd096de6c08bc00a524d47
Carlos Mestre <carmesce@gmail.com>
2024-10-05 16:57:57 +0200

    Revert "Secret env File"
    This reverts commit 91eb11da9e1337fa5ce9952eee38128da3ae3778.

--- envFichero.js
+++ envFichero.js
@@ -1,12 +1,3 @@
-// LEE el contenido del fichero .env y lo guarda en process.env
 require('dotenv').config()
 
-
 console.log(process.env.POKEMON)
-
-let connect = {
-	user: process.env.DB_USER,
-	pass: process.env.DB_PASS
-}
-
-console.log(connect)
